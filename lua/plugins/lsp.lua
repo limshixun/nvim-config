d7dd6019d3ccbbfb0a9af2a3d742721a50ea38df
limshixun <limshixun1106@gmail.com>
2026-04-21 23:38:51 +0800

    Add mason & WIP web component language server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 return {
 	{'neovim/nvim-lspconfig',
 	config = function()
+		-- lua lsp
 		vim.lsp.config('lua_ls', {
 			cmd = {'lua-language-server' },
 			filetypes = {'lua'},
@@ -14,6 +15,14 @@ return {
 			}
 		})
 		vim.lsp.enable('lua_ls')
+		-- web component lsp
+		vim.lsp.config('wc_language_server', {
+			cmd = {'wc-language-server', '--stdio'},
+			filetypes = { "html", "javascriptreact", "typescriptreact", "astro", "svelte", "vue", "markdown", "mdx", "javascript", "typescript", "css", "scss", "less" },
+			init_options = {hostInfo = "neovim"},
+			root_markers = { "wc.config.js", "wc.config.ts", "wc.config.mjs", "wc.config.cjs", "custom-elements.json", "package.json", ".git" }
+		})
+		vim.lsp.enable('wc_language_server')
 	end
 },
 	{'hrsh7th/nvim-cmp'},
