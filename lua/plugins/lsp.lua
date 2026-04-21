42493c8379e260685823a3e6f001c52f68c52845
limshixun <limshixun1106@gmail.com>
2026-04-21 21:09:11 +0800

    Add lua_ls lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,31 @@
 return {
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/nvim-cmp'}
+	{'neovim/nvim-lspconfig',
+	config = function()
+		vim.lsp.config('lua_ls', {
+			cmd = {'lua-language-server' },
+			filetypes = {'lua'},
+			root_markers = {{ '.luarc.json', '.luarc.jsonc' }, '.git' },
+			settings = {
+				Lua = {
+					runtime = {
+						version = 'LuaJIT',
+					}
+				}
+			}
+		})
+		vim.lsp.enable('lua_ls')
+	end
+},
+	{'hrsh7th/nvim-cmp'},
+{
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 }
