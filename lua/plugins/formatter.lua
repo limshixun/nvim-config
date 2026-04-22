8b2984a5ce510530d549158fd0f3aff10ec3b1f6
limshixun <limshixun1106@gmail.com>
2026-04-22 21:41:57 +0800

    Format configs

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,7 +5,6 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd", "prettier", stop_after_first = true },
 			},
 			format_on_save = {
 				timeout_ms = 500,
