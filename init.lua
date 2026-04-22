07267e47b5af7f2b1ba71f17747a52706db87a72
limshixun <limshixun1106@gmail.com>
2026-04-22 20:58:39 +0800

    Format lua plugins and configs files

--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@ require("config.lazy")
 require("toggleterm").setup()
 
 -- Quick Save and Quit
-vim.keymap.set({'n','i'}, '<F5>', ':wq<cr>', { desc = 'Save file'})
+vim.keymap.set({ 'n', 'i' }, '<F5>', ':wq<cr>', { desc = 'Save file' })
 
 -- Escape terminal
-vim.keymap.set('t','<Esc>', [[<C-\><C-n>]], { noremap = true })
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true })
 
 -- Yank to clipboard
 vim.opt.clipboard = "unnamedplus"
 
 -- Indentation Config
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 
 -- Line number
 vim.opt.number = true
--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,43 +1,40 @@
 return {
-  {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", "nvim-mini/mini.nvim" },
-    lazy = false,
-    keys = {
-      {
-        "<space><Tab>",
-        function()
-			local oil = require("oil")
-			oil.toggle_float(nil, {preview = { vertical = true}})
-        end,
-        desc = "Toggle Oil in vsplit",
-      },
-    },
-    opts = {
-      default_file_explorer = true,
-      columns = {
-        "icon",
-        "permissions",
-        "size",
-      },
-      view_options = {
-        show_hidden = true,
-		is_always_hidden = function(name, _)
-			return name == ".."
-		end,
-      },
-	  float = {
-		  padding = 2,
-		  max_width = 0.7,
-		  preview_split = "right",
-		  border = "rounded",
-	  },
-	  preview_win = {
-		  update_on_cursor_move = true,
-	  },
-    },
-  },
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", "nvim-mini/mini.nvim" },
+		lazy = false,
+		keys = {
+			{
+				"<space><Tab>",
+				function()
+					local oil = require("oil")
+					oil.toggle_float(nil, { preview = { vertical = true } })
+				end,
+				desc = "Toggle Oil in vsplit",
+			},
+		},
+		opts = {
+			default_file_explorer = true,
+			columns = {
+				"icon",
+				"permissions",
+				"size",
+			},
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					return name == ".."
+				end,
+			},
+			float = {
+				padding = 2,
+				max_width = 0.7,
+				preview_split = "right",
+				border = "rounded",
+			},
+			preview_win = {
+				update_on_cursor_move = true,
+			},
+		},
+	},
 }
-
-
-
