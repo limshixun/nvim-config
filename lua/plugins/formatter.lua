23d38c20c677ad28e28ba0d911682a9275e2bbf2
limshixun <limshixun1106@gmail.com>
2026-04-22 22:38:51 +0800

    Add json formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,6 +5,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				json = { 'jq' },
 			},
 			format_on_save = {
 				timeout_ms = 500,
