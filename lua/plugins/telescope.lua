c5970203dffc3f020c1b813e7b8b3e9647e4d281
limshixun <limshixun1106@gmail.com>
2026-04-22 20:00:28 +0800

    Add normal mode <space>lg to quick Telescope live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,11 +4,12 @@ return {
 		'BurntSushi/ripgrep',
 		'sharkdp/fd',
 		'nvim-lua/plenary.nvim',
-		{'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },	
+		{'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 		{'nvim-tree/nvim-web-devicons', lazy = false, },
 	},
 	keys = {
 		{"<space>fd", "<cmd>Telescope find_files<cr>"},
+		{"<space>lg", "<cmd>Telescope live_grep<cr>"},
 		{"<space>nc", function()
 			require('telescope.builtin').find_files { cwd = vim.fn.stdpath("config")}
 		end
